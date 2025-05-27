218a49c63bf0d113acfd341a4608d639a6d17fe5
Emanuel Cesar <165450532+Emanuel71@users.noreply.github.com>
2025-05-27 16:19:44 -0300

    Update and rename 31 PARTITION BY.sql to  PARTITION BY.sql

--- PARTITION BY.sql
+++ PARTITION BY.sql
@@ -8,6 +8,6 @@ SELECT
 	,Bytes
 	,UnitPrice
 	,ROW_NUMBER() OVER(ORDER BY Milliseconds ASC) AS 'ROW_NUMBER' -- aqui rankeamos por tamanho da musica
-	,ROW_NUMBER() OVER(PARTITION BY Composer ORDER BY Milliseconds ASC) AS 'ROW NUMBER_PARTITION BY' -- aqui fizemos a separação por compositor e rankeamos
+	,ROW_NUMBER() OVER(PARTITION BY Composer ORDER BY Milliseconds ASC) AS 'ROW NUMBER_PARTITION BY' -- aqui fizemos a separaÃ§Ã£o por compositor e rankeamos
 FROM Track t
 WHERE Composer IS NOT NULL -- remover os null
